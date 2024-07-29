6d2282eac4c428e997a909a0028be57b517a1fd3
George Kariuki <32328769+ggeorgekkariuki@users.noreply.github.com>
2024-07-29 18:50:27 +0000

    SAdd new treamluit applicatiion file

---
+++ streamlit_run.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Company Logo
+st.sidebar.image('Images/image4.png', use_column_width=True)
+
+# Sidebar Selectbox initialization
+selected = st.sidebar.selectbox("Go to", ["Home", "About", "Contact"])
+
+if selected == "Home":
+    with open('home_page.py', 'r') as file:
+        code = file.read()
+        exec(code)
