1b651bf7c0a25cda0438cde3ce15501b3ea7a546
AtomHarris <162112289+AtomHarris@users.noreply.github.com>
2024-07-29 09:11:38 +0300

    Updating TEACH button

--- app.py
+++ app.py
@@ -313,7 +313,7 @@ if selected == "Home":
                     if st.button("Confirm Correction"):
                         if correct_class:
                             move_image_to_correct_folder(image, correct_class) 
-                            if st.button("Teach Model"):
+                            if st.button("TEACH"):
                                 st.write("Kindly Be Patient......")
                                 st.write("This will take up to an hour!!!")
                                 retrain_model()
