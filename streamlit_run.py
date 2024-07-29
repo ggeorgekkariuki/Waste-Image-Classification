b9b2bc615f4ceb9186d66e472f131c82bc50a64f
George Kariuki <32328769+ggeorgekkariuki@users.noreply.github.com>
2024-07-29 19:10:07 +0000

    Add About and Contact pages

--- streamlit_run.py
+++ streamlit_run.py
@@ -10,3 +10,25 @@ if selected == "Home":
     with open('main_page.py', 'r') as file:
         code = file.read()
         exec(code)
+
+elif selected == "About":
+    tab1, tab2 = st.tabs(["Software Used", "Hardware Used"])
+
+    with tab1:
+        st.header("Software Description", divider=True)
+        st.write("COMING SOON......")
+
+    with tab2:
+        st.header("Hardware Description", divider=True)
+        st.write("COMING SOON......")
+
+elif selected == "Contact":
+    st.header("DEVELOPER CONTACTS", divider= "rainbow")
+    with st.container(border= True):
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.write("Annabellah Mbungu   \nannbellah.mbungu@student.moringaschool.com     \nNumber:  ")  
+        with col2:
+            st.write("Brian Muthama  \nbrian.muthama@student.moringaschool.com   \nNumber:  ")
+        with col3:
+            st.write("Harris Lukundi  \nharris.lukundi@student.moringaschool.com  \nNumber:  ")
